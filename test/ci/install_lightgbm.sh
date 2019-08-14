c58f1f218c429cb25ab6564eead0a2cf180120c1
Andrew Kane <andrew@chartkick.com>
2019-08-13 20:54:08 -0700

    Attempt to fix Travis

--- test/ci/install_lightgbm.sh
+++ test/ci/install_lightgbm.sh
@@ -5,9 +5,8 @@ set -e
 CACHE_DIR=$HOME/lightgbm/$LIGHTGBM_VERSION
 
 if [ ! -d "$CACHE_DIR" ]; then
-  wget https://github.com/microsoft/LightGBM/releases/download/v$LIGHTGBM_VERSION/lightgbm-$LIGHTGBM_VERSION.tar.gz
-  tar xvfz lightgbm-$LIGHTGBM_VERSION.tar.gz
-  mv lightgbm-$LIGHTGBM_VERSION $CACHE_DIR
+  git clone --recursive https://github.com/microsoft/LightGBM
+  mv LightGBM $CACHE_DIR
   cd $CACHE_DIR
   mkdir build
   cd build
