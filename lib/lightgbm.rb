1386150b4960d15f0840260d84b66b1e992f2500
Andrew Kane <andrew@chartkick.com>
2019-08-14 18:18:43 -0700

    Verbose eval option

--- lib/lightgbm.rb
+++ lib/lightgbm.rb
@@ -84,7 +84,7 @@ module LightGBM
       booster
     end
 
-    def cv(params, train_set, num_boost_round: 100, nfold: 5, seed: 0, shuffle: true)
+    def cv(params, train_set, num_boost_round: 100, nfold: 5, seed: 0, shuffle: true, verbose_eval: nil)
       rand_idx = (0...train_set.num_data).to_a
       rand_idx.shuffle!(random: Random.new(seed)) if shuffle
 
@@ -119,7 +119,7 @@ module LightGBM
         eval_hist["l2-mean"] << mean
         eval_hist["l2-stdv"] << stdev
 
-        puts "[#{iteration + 1}]\tcv_agg's l2: %g + %g" % [mean, stdev]
+        puts "[#{iteration + 1}]\tcv_agg's l2: %g + %g" % [mean, stdev] if verbose_eval
       end
 
       eval_hist
