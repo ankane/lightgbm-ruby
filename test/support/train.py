f9043d645fc8be527b0366b64c3e8d20656dd1d9
Andrew Kane <andrew@chartkick.com>
2020-05-09 03:00:24 -0700

    Better output [skip ci]

--- test/support/train.py
+++ test/support/train.py
@@ -14,7 +14,7 @@ y_test = y[300:]
 
 print('test_regression')
 
-regression_params = {'objective': 'regression'}
+regression_params = {'objective': 'regression', 'verbosity': -1}
 regression_train = lgb.Dataset(X_train, label=y_train)
 regression_test = lgb.Dataset(X_test, label=y_test)
 bst = lgb.train(regression_params, regression_train, valid_sets=[regression_train, regression_test], verbose_eval=False)
@@ -24,7 +24,7 @@ print(np.sqrt(np.mean((y_pred - y_test)**2)))
 print('')
 print('test_binary')
 
-binary_params = {'objective': 'binary'}
+binary_params = {'objective': 'binary', 'verbosity': -1}
 binary_train = lgb.Dataset(X_train, label=y_train.replace(2, 1))
 binary_test = lgb.Dataset(X_test, label=y_test.replace(2, 1))
 bst = lgb.train(binary_params, binary_train, valid_sets=[binary_train, binary_test], verbose_eval=False)
@@ -34,7 +34,7 @@ print(y_pred[0])
 print('')
 print('test_multiclass')
 
-multiclass_params = {'objective': 'multiclass', 'num_class': 3}
+multiclass_params = {'objective': 'multiclass', 'num_class': 3, 'verbosity': -1}
 multiclass_train = lgb.Dataset(X_train, label=y_train)
 multiclass_test = lgb.Dataset(X_test, label=y_test)
 bst = lgb.train(multiclass_params, multiclass_train, valid_sets=[multiclass_train, multiclass_test], verbose_eval=False)
@@ -57,7 +57,7 @@ print(bst.best_iteration)
 print('')
 print('test_early_stopping_early_higher_better')
 
-params = {'objective': 'binary', 'metric': 'auc'}
+params = {'objective': 'binary', 'metric': 'auc', 'verbosity': -1}
 bst = lgb.train(params, binary_train, valid_sets=[binary_train, binary_test], early_stopping_rounds=5, verbose_eval=False)
 print(bst.best_iteration)
 
